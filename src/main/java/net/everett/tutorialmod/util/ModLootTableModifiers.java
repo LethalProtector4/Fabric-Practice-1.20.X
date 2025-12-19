f48b32d7c278e9d8d442ea114ad30460dd2ef1d5
Everett <everettneu44@gmail.com>
2025-12-18 23:48:55 -0500

    did adding items to sus sand tutorial

--- src/main/java/net/everett/tutorialmod/util/ModLootTableModifiers.java
+++ src/main/java/net/everett/tutorialmod/util/ModLootTableModifiers.java
@@ -3,12 +3,18 @@ package net.everett.tutorialmod.util;
 import net.everett.tutorialmod.item.ModItems;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTable;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.entry.LootPoolEntry;
 import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 import net.minecraft.util.Identifier;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ModLootTableModifiers {
     private static final Identifier JUNGLE_TEMPLE_ID =
@@ -17,6 +23,9 @@ public class ModLootTableModifiers {
             new Identifier("minecraft", "entities/creeper");
     private static final Identifier ZOMBIE_ID =
             new Identifier("minecraft", "entities/zombie");
+
+    private static final Identifier SUSPICIOUS_SAND_ID =
+            new Identifier("minecraft", "archaeology/desert_pyramid");
 
     public static void modifyLootTables() {
         LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
@@ -49,6 +58,18 @@ public class ModLootTableModifiers {
 
                 tableBuilder.pool(poolBuilder.build());
             }
+        });
+
+        LootTableEvents.REPLACE.register((resourceManager, lootManager, id, original, source) -> {
+            if(SUSPICIOUS_SAND_ID.equals(id)) {
+                List<LootPoolEntry> entries = new ArrayList<>(Arrays.asList(original.pools[0].entries));
+                entries.add(ItemEntry.builder(ModItems.METAL_DETECTOR).build());
+
+                LootPool.Builder pool = LootPool.builder().with(entries);
+                return LootTable.builder().pool(pool).build();
+            }
+
+            return null;
         });
     }
 }
